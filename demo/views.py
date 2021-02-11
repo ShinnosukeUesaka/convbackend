38301c0ec3e28e93f1fa240859ba854fb8398d3a
Ken Shibata <colourdelete@gmail.com>
2021-02-10 23:23:41 -0500

    fix typo

--- demo/views.py
+++ demo/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
+
 def chat(request, conversation_id):
     if request.method == 'post':
         data = json.loads(request.body)
@@ -18,25 +19,24 @@ def chat(request, conversation_id):
         logitem_human = LogItem.objects.create(log=human, text=data['user_input'], name_text=scenario.human_name)
         logitem_human.save()
 
-
-        log_text = generate_log_text(conversation)
+        log_text = prepare_log_text(conversation)
         response = gpt(log_text)
-
 
         logitem_ai = LogItem.objects.create(log=log, text=response, name_text=scenario.ai_name)
         logitem_ai.save()
 
         return JsonResponse({'response': serialize(logitem_ai)})
 
+
 @csrf_exempt
 def conversations_view(request):
     if request.method == 'POST':
-        #create new conversation
+        # create new conversation
         print(json.loads(request.body))
         scenario = Scenario.objects.get(pk=json.loads(request.body)['scenario_id'])
         conversation = Conversation.objects.create(scenario=scenario)
-        log = Log.objects.create(conversation = conversation)
-        log_item = LogItem.objects.create(log=log, type = LogItem.Type.INITIAL_PROMPT, text=scenario.initial_prompt)
+        log = Log.objects.create(conversation=conversation)
+        log_item = LogItem.objects.create(log=log, type=LogItem.Type.INITIAL_PROMPT, text=scenario.initial_prompt)
 
         conversation.save
         log.save
@@ -46,14 +46,12 @@ def conversations_view(request):
 
 
 def log_view(request, conversation_id):
-    log_tems = LogItem.objects.filter(log__conversation__id=conversation_id).filter(is_visible=True)
+    log_items = LogItem.objects.filter(log__conversation__id=conversation_id).filter(is_visible=True)
     return serialize(log_items)
 
 
 def edit_log(request):
     return
-
-
 
 
 # 以降 tools
@@ -71,7 +69,7 @@ def prepare_log_text(conversation):
         if logtype == LogItem.Type.AI or logtype == LogItem.Type.HUMAN:
             logtext += log.name_text + ": " + log.log_text + "\n"
         elif logtype == LogItem.Type.INITIAL_PROMPT or logtype == LogItem.Type.NARRATION:
-            logtext += log.text  + "\n"
+            logtext += log.text + "\n"
 
     logtext += conversation.scenario_id.ai_name + ": "
 

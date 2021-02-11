e685e60596cfce2a2bd1ef485b63f5f2e701ecb7
Ken Shibata <colourdelete@gmail.com>
2021-02-10 23:14:55 -0500

    add comments and intellij config files

--- demo/models.py
+++ demo/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
-#https://www.webforefront.com/django/modeldatatypesandvalidation.html
+# https://www.webforefront.com/django/modeldatatypesandvalidation.html
 
 class Scenario(models.Model):
     title = models.CharField(max_length=50)
@@ -9,30 +10,29 @@ class Scenario(models.Model):
     ai_name = models.CharField(max_length=20)
     human_name = models.CharField(max_length=20)
     summarize_token = models.IntegerField()
-    info = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    response_length = models.IntegerField(default=150)
+    info = models.CharField(max_length=100)  # eg place: cafe, mission: buy coffee
+    description = models.CharField(max_length=100)  # scenario description
+
+    # GPT-3 Settings
+    response_length = models.IntegerField(default=150)  # ai response length
     temperature = models.DecimalField(max_digits=4, decimal_places=3, default=0.9)
     top_p = models.DecimalField(max_digits=4, decimal_places=3, default=1)
     frequency_penalty = models.DecimalField(max_digits=4, decimal_places=3, default=0)
     presence_penalty = models.DecimalField(max_digits=4, decimal_places=3, default=0.6)
 
-
-
-
     class Duration(models.IntegerChoices):
-        #https://docs.djangoproject.com/en/3.0/ref/models/fields/#enumeration-types
-        Five = 5, '5分'
-        TEN = 0,'10分'
-        TWENTY = 20,'20分'
+        # https://docs.djangoproject.com/en/3.0/ref/models/fields/#enumeration-types
+        FIVE = 5, '5分'
+        TEN = 0, '10分'
+        TWENTY = 20, '20分'
 
     duration = models.IntegerField(choices=Duration.choices)
 
     class Level(models.IntegerChoices):
-        #https://docs.djangoproject.com/en/3.0/ref/models/fields/#enumeration-types
-        BEGINNER = 1,'初心者'
-        INTERMEDIATE = 2,'中級者'
-        ADVANCED = 3,'上級者'
+        # https://docs.djangoproject.com/en/3.0/ref/models/fields/#enumeration-types
+        BEGINNER = 1, '初心者'
+        INTERMEDIATE = 2, '中級者'
+        ADVANCED = 3, '上級者'
 
     level = models.IntegerField(choices=Level.choices)
 
@@ -48,6 +48,7 @@ class Option(models.Model):
     def __str__(self):
         return self.name
 
+
 class OptioinItem(models.Model):
     option = models.ForeignKey(Option, on_delete=models.CASCADE)
 
@@ -56,17 +57,20 @@ class OptioinItem(models.Model):
     def __str__(self):
         return self.name
 
+
 class Conversation(models.Model):
     scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.scenario.title
 
+
 class Log(models.Model):
     conversation = models.OneToOneField(Conversation, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.conversation.scenario.title
+
 
 class LogItem(models.Model):
     log = models.ForeignKey(Log, on_delete=models.CASCADE)
@@ -77,7 +81,7 @@ class LogItem(models.Model):
     is_visible = models.BooleanField(default=True)
 
     class Type(models.IntegerChoices):
-        #https://docs.djangoproject.com/en/3.0/ref/models/fields/#enumeration-types
+        # https://docs.djangoproject.com/en/3.0/ref/models/fields/#enumeration-types
         INITIAL_PROMPT = 1
         NARRATION = 2
         AI = 3

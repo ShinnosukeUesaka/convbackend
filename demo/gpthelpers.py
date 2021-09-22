cc97be90731b6d66c323fefce3d5b323696eb102
ShinnosukeUesaka <45286939+ShinnosukeUesaka@users.noreply.github.com>
2021-09-23 08:21:41 +0900

    add convert_questions_to_full_question

--- demo/gpthelpers.py
+++ demo/gpthelpers.py
@@ -21,7 +21,6 @@ Correct or Incorrect:"""
     else: # error
         return True
 
-
 def correct_english(broken_english, context=None) -> str:
     # move the examples to somewhere easily editable.　#https://www.eibunkousei.net/%E6%97%A5%E6%9C%AC%E4%BA%BA%E3%81%AE%E8%8B%B1%E8%AA%9E%E3%81%AB%E3%82%88%E3%81%8F%E3%81%82%E3%82%8B%E9%96%93%E9%81%95%E3%81%84/
     examples =  """BrokenEnglish: Its like that i'm chat with a really person not robot.
@@ -71,7 +70,7 @@ GoodEnglish: Let's have breakfast together tomorrow.
     else:
         return good_english
 
-def define_word(word) -> str:
+def define_word(word):
 
     PROMPT = """Define the word and use the word in a sentence.
 
@@ -99,3 +98,24 @@ Word: """
         definition, example = "error", "error"
 
     return definition, example
+
+def convert_questions_to_full_question(question, context) -> str:
+
+    PROMPT = """I am studying mathematics at a university.
+Why did you decide to majorar that?
+Full Question: Why did you decide to major in mathematics at your university?
+
+"""
+    input = PROMPT + context + "\n" + question + "\n" + "Full Question:"
+
+    output =  completion(engine='davinci', prompt_=input,
+    temperature = 0,
+    max_tokens = 300,
+    top_p = 1,
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    stop=["\n"])
+
+    output = output[1:]
+
+    return output

d90685bea2e05a49e69b607c831b064c21928c20
ShinnosukeUesaka <45286939+ShinnosukeUesaka@users.noreply.github.com>
2021-09-25 21:38:37 +0900

    edit gpt parameters

--- demo/gpt3.py
+++ demo/gpt3.py
@@ -41,7 +41,7 @@ def make_header() -> dict:
 def completion(
         prompt_: str,
         engine: str = 'davinci',
-        temperature: float = 0.7,
+        temperature: float = 0.5,
         max_tokens: int = 172,
         top_p: float = 1,
         frequency_penalty: float = 0,

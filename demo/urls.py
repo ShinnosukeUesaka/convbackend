aa377a8072c7a5b08eb8d5c49639527c884c9c74
ShinnosukeUesaka <45286939+ShinnosukeUesaka@users.noreply.github.com>
2021-10-08 17:02:46 +0900

    edit url

--- demo/urls.py
+++ demo/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('conversation/', views.conversations_view, name='conversations_view'),
-    path('conversation/scenario', views.scenario, name='scenario'),
     path('conversation/scenarios', views.scenarios, name='scenarios'),
     path('conversation/chat/', views.chat, name='chat'),
-    path('conversation/log/view', views.log_view, name='log_view'),
     path('dictionary', views.dictionary, name='dictionary'),
+
+    path('conversation/log/view', views.log_view, name='log_view'), # Not used
+    path('conversation/', views.conversations_view, name='conversations_view'), # Not used
+    path('conversation/scenario', views.scenario, name='scenario'), # Not used
 ]
